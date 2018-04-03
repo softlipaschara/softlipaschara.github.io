4d1b8ef2247aa262c738ac605a5b4c3ae2b6a050
softlipaschara <yangxinyue@yangxinyues-MacBook-Pro.local>
2018-04-03 22:37:36 +0200

    add blog part

--- hideContent.js
+++ hideContent.js
@@ -13,10 +13,12 @@ window.onload = function(){
         document.getElementById("index").style.display = 'none';
         document.getElementById("about").style.display = 'block';
         document.getElementById("contact").style.display = 'none';
+        document.getElementById("blog").style.display = 'none';
         document.getElementById("mask").className = "maskWhite";
         document.getElementById("indexTitle").className = "aboutActive";
         document.getElementById("aboutTitle").className = "aboutActive";
         document.getElementById("contactTitle").className = "aboutActive";
+        document.getElementById("blogTitle").className = "aboutActive";
         document.getElementById("waterImage").style.display = 'none';
         document.getElementById("besidesPart").style.display = 'block';
         return false;
@@ -27,12 +29,14 @@ window.onload = function(){
         //document.getElementById("printPart").style.display = 'none';
         document.getElementById("index").style.display = 'none';
         document.getElementById("about").style.display = 'none';
+        document.getElementById("blog").style.display = 'none';
         document.getElementById("contact").style.display = 'block';
         document.getElementById("waterImage").style.display = 'block';
         document.getElementById("mask").className = "maskWhite";
         document.getElementById("indexTitle").className = "contactActive";
         document.getElementById("aboutTitle").className = "contactActive";
         document.getElementById("contactTitle").className = "contactActive";
+        document.getElementById("blogTitle").className = "contactActive";
         document.getElementById("besidesPart").style.display = 'none';
 
         return false;
@@ -41,11 +45,29 @@ window.onload = function(){
     function showIndex(){
         document.getElementById("index").style.display = 'block';
         document.getElementById("about").style.display = 'none';
+        document.getElementById("blog").style.display = 'none';
         document.getElementById("contact").style.display = 'none';
         document.getElementById("mask").className = "maskBlack";
         document.getElementById("indexTitle").className = "indexActive";
         document.getElementById("aboutTitle").className = "indexActive";
         document.getElementById("contactTitle").className = "indexActive";
+        document.getElementById("blogTitle").className = "indexActive";
+        document.getElementById("waterImage").style.display = 'none';
+        document.getElementById("besidesPart").style.display = 'none';
+        //document.getElementById("printPart").style.display = 'none';
+        return false;
+    }
+
+    function showBlog(){
+        document.getElementById("index").style.display = 'none';
+        document.getElementById("about").style.display = 'none';
+        document.getElementById("blog").style.display = 'block';
+        document.getElementById("contact").style.display = 'none';
+        document.getElementById("mask").className = "maskWhite";
+        document.getElementById("indexTitle").className = "blogActive";
+        document.getElementById("aboutTitle").className = "blogActive";
+        document.getElementById("contactTitle").className = "blogActive";
+        document.getElementById("blogTitle").className = "blogActive";
         document.getElementById("waterImage").style.display = 'none';
         document.getElementById("besidesPart").style.display = 'none';
         //document.getElementById("printPart").style.display = 'none';
@@ -64,6 +86,9 @@ window.onload = function(){
                 break;
             case "#selectedWorks":
                 showIndex();
+                break;
+            case "#blog":
+                showBlog();
                 break;
             default:
                 showIndex();
@@ -88,6 +113,11 @@ window.onload = function(){
     var indexButton = document.getElementById("indexButton");
     indexButton.addEventListener("click", function(){
         location.hash = "selectedWorks"
+    });
+
+    var blogButton = document.getElementById("blogButton");
+    blogButton.addEventListener("click", function(){
+        location.hash = "blog"
     });
 
 
